17160a805d688c58e107584d46c6b4197c8d3a81
Komori Yuichiro <mail.cmmmli@gmail.com>
2024-03-25 13:05:19 +0900

    chore(web): add hoge & fuga pages

---
+++ app/web/app/fuga/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Fuga = () => {
+  return (
+    <>
+      <h1>app/fuga</h1>
+      <Link href="/hoge">Go to Hoge</Link>
+    </>
+  );
+};
+
+export default Fuga;
---
+++ app/web/app/hoge/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Hoge = () => {
+  return (
+    <>
+      <h1>app/hoge</h1>
+      <Link href="/fuga">Go to Fuga</Link>
+    </>
+  );
+};
+
+export default Hoge;
