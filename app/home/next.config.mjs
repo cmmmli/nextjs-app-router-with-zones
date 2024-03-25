7b58f9cd7db7c392050d08d1b1d90390a30921d3
Komori Yuichiro <mail.cmmmli@gmail.com>
2024-03-25 13:05:03 +0900

    chore(home): setting rewites

--- app/home/next.config.mjs
+++ app/home/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    const web_url = process.env.WEB_URL || "http://localhost:3001";
+
+    return [
+      {
+        source: "/web",
+        destination: `${web_url}/web`,
+      },
+      {
+        source: "/web/:path*",
+        destination: `${web_url}/web/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
